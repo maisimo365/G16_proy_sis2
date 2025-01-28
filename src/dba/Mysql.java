d260a65249732a090b0f2af8882232b270be0c6c
Esther2419 <romeromenachoesther@gmail.com>
2025-01-28 08:39:20 -0400

    First Commit

---
+++ src/dba/Mysql.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dba;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Esther
+ */
+public class Mysql {
+    public static Connection getConnection(){
+         Connection cn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/tablasAux", "root", "");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }catch(Exception e1){
+            System.out.println("Error: " + e1.getMessage());
+        }
+    return  cn;
+    }
+    
+}

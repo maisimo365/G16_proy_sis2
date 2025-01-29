16b20fdb51f44d2fcc0454302cbaf013b7ccb484
Nicolas447-4 <nzapata470@gmail.com>
2025-01-28 21:59:02 -0400

    Compartiendo el codigo que se me dio a trabajar

---
+++ src/main/java/com/mycompany/nicolas/Nicolas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nicolas;
+
+/**
+ *
+ * @author nzapa
+ */
+public class Nicolas {
+
+    public static void main(String[] args) {
+        new registroProfesor().setVisible(true);
+    }
+}
